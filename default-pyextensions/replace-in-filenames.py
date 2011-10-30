4126caf5dd5b8ba7ec66ecdacdd2a878b932c793
Giuseppe Penone <giuseppe.penone@qsdsistemi.it>
2011-10-31 00:02:19 +0100

    working on 'replace-in-filenames.py' for nautilus 3

--- default-pyextensions/replace-in-filenames.py
+++ default-pyextensions/replace-in-filenames.py
@@ -4,7 +4,7 @@
 """This module adds a menu item to the nautilus right-click menu which allows to Replace a String
    with another one in all Current/Selected Directory Filenames just through the right-clicking"""
 
-#   replace-in-filenames.py version 1.2.2
+#   replace-in-filenames.py version 3.0
 #
 #   Copyright 2011 Giuseppe Penone <giuspen@gmail.com>
 #
@@ -23,8 +23,9 @@
 #   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #   MA 02110-1301, USA.
 
-import gtk
-import nautilus, urllib, os
+from gi.repository import Nautilus, GObject
+#import gtk
+import urllib, os
 import locale, gettext
 
 APP_NAME = "nautilus-pyextensions"
@@ -35,33 +36,9 @@ gettext.bindtextdomain(APP_NAME, LOCALE_PATH)
 gettext.textdomain(APP_NAME)
 _ = gettext.gettext
 # post internationalization code starts here
-GLADE_PATH = "/usr/share/nautilus-pyextensions/glade/nautilus-pyextensions_replace.glade"
 
 
-class GladeWidgetsWrapper:
-    """Handles the retrieval of glade widgets"""
-
-    def __init__(self, glade_file_path, gui_instance):
-        try:
-            self.glade_widgets = gtk.Builder()
-            self.glade_widgets.set_translation_domain(APP_NAME)
-            self.glade_widgets.add_from_file(glade_file_path)
-            self.glade_widgets.connect_signals(gui_instance)
-        except: print "Failed to load the glade file"
-
-    def __getitem__(self, key):
-        """Gives us the ability to do: wrapper['widget_name'].action()"""
-        return self.glade_widgets.get_object(key)
-
-    def __getattr__(self, attr):
-        """Gives us the ability to do: wrapper.widget_name.action()"""
-        new_widget = self.glade_widgets.get_object(attr)
-        if new_widget is None: raise AttributeError, 'Widget %r not found' % attr
-        setattr(self, attr, new_widget)
-        return new_widget
-
-
-class ReplaceInFilenames(nautilus.MenuProvider):
+class ReplaceInFilenames(GObject.GObject, Nautilus.MenuProvider):
     """Implements the 'Replace in Filenames' extension to the nautilus right-click menu"""
 
     def __init__(self):
@@ -74,42 +51,57 @@ class ReplaceInFilenames(nautilus.MenuProvider):
         if len(uri_raw) < 7: return
         curr_dir = urllib.unquote(uri_raw[7:])
         if os.path.isfile(curr_dir): curr_dir = os.path.dirname(curr_dir)
-        self.glade = GladeWidgetsWrapper(GLADE_PATH, self)
-        self.glade.replacedialog.connect('key_press_event', self.on_key_press)
-        response = self.glade.replacedialog.run()
-        self.glade.replacedialog.hide()
-        if response != 1: return
-        replace_from = self.glade.entry_replace_from.get_text()
-        replace_to = self.glade.entry_replace_to.get_text()
-        for old_name in os.listdir(curr_dir):
-            old_filename = os.path.join(curr_dir, old_name)
-            if os.path.isfile(old_filename):
-                new_name = old_name.replace(replace_from, replace_to)
-                if new_name != old_name:
-                    os.rename(old_filename, os.path.join(curr_dir, new_name))
-
-    def on_key_press(self, widget, event):
-        """Catches AnchorHandle Dialog key presses"""
-        keyname = gtk.gdk.keyval_name(event.keyval)
-        if keyname == "Return": self.glade.replace_button_ok.clicked()
+        #dialog = gtk.Dialog(title=_("Replace in Filenames"),
+                            #buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
+                                     #gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+        #dialog.set_position(gtk.WIN_POS_CENTER)
+        #entry_find = gtk.Entry()
+        #entry_replace = gtk.Entry()
+        #frame_find = gtk.Frame(label="<b>"+_("Replace...")+"</b>")
+        #frame_find.get_label_widget().set_use_markup(True)
+        #frame_find.add(entry_find)
+        #frame_replace = gtk.Frame(label="<b>"+_("...with")+"</b>")
+        #frame_replace.get_label_widget().set_use_markup(True)
+        #frame_replace.add(entry_replace)
+        #content_area = dialog.get_content_area()
+        #content_area.pack_start(frame_find)
+        #content_area.pack_start(frame_replace)
+        #content_area.show_all()
+        #def on_key_press_dialog(widget, event):
+            #if gtk.gdk.keyval_name(event.keyval) == "Return":
+                #button_box = dialog.get_action_area()
+                #buttons = button_box.get_children()
+                #buttons[0].clicked() # first is the ok button
+        #dialog.connect("key_press_event", on_key_press_dialog)
+        #response = dialog.run()
+        #dialog.hide()
+        #if response != gtk.RESPONSE_ACCEPT: return
+        #replace_from = entry_find.get_text()
+        #replace_to = entry_replace.get_text()
+        #for old_name in os.listdir(curr_dir):
+            #old_filename = os.path.join(curr_dir, old_name)
+            #if os.path.isfile(old_filename):
+                #new_name = old_name.replace(replace_from, replace_to)
+                #if new_name != old_name:
+                    #os.rename(old_filename, os.path.join(curr_dir, new_name))
 
     def get_file_items(self, window, sel_items):
         """Adds the 'Replace in Filenames' menu item to the Nautilus right-click menu,
            connects its 'activate' signal to the 'run' method passing the selected Directory/File"""
         if len(sel_items) != 1 or sel_items[0].get_uri_scheme() != 'file': return
-        item = nautilus.MenuItem('NautilusPython::gtk-find-and-replace',
-                                 _('Replace in Filenames'),
-                                 _('Replace in Filenames of the Current/Selected Directory') )
-        item.set_property('icon', 'gtk-find-and-replace')
+        item = Nautilus.MenuItem(name='NautilusPython::gtk-find-and-replace',
+                                 label=_('Replace in Filenames'),
+                                 tip=_('Replace in Filenames of the Current/Selected Directory'),
+                                 icon='gtk-find-and-replace')
         item.connect('activate', self.run, sel_items[0])
         return item,
 
     def get_background_items(self, window, current_directory):
         """Adds the 'Replace in Filenames' menu item to the Nautilus right-click menu,
            connects its 'activate' signal to the 'run' method passing the current Directory"""
-        item = nautilus.MenuItem('NautilusPython::gtk-find-and-replace',
-                                 _('Replace in Filenames'),
-                                 _('Replace in Filenames of the Current Directory') )
-        item.set_property('icon', 'gtk-find-and-replace')
+        item = Nautilus.MenuItem(name='NautilusPython::gtk-find-and-replace',
+                                 label=_('Replace in Filenames'),
+                                 tip=_('Replace in Filenames of the Current Directory'),
+                                 icon='gtk-find-and-replace')
         item.connect('activate', self.run, current_directory)
         return item,
