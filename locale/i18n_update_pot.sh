767cd622eecab906dc8d5b9cc64f27e4886d67f7
Giuseppe Penone <giuseppe.penone@qsdsistemi.it>
2011-11-02 00:06:00 +0100

    updated languages files

--- locale/i18n_update_pot.sh
+++ locale/i18n_update_pot.sh
@@ -3,6 +3,5 @@
 cd ..
 
 intltool-extract --type=gettext/glade glade/nautilus-pyextensions.glade
-intltool-extract --type=gettext/glade glade/nautilus-pyextensions_replace.glade
 
-xgettext --language=Python --keyword=_ --keyword=N_ --output=locale/nautilus-pyextensions.pot modules/core.py modules/cons.py glade/nautilus-pyextensions.glade.h glade/nautilus-pyextensions_replace.glade.h default-pyextensions/add-to-audacious2-playlist.py default-pyextensions/meld-compare.py default-pyextensions/kdiff3-compare.py default-pyextensions/open-as-root.py default-pyextensions/open-terminal-geometry.py default-pyextensions/replace-in-filenames.py default-pyextensions/set-as-desktop-background.py
+xgettext --language=Python --from-code=utf-8 --keyword=_ --keyword=N_ --output=locale/nautilus-pyextensions.pot modules/core.py modules/cons.py glade/nautilus-pyextensions.glade.h default-pyextensions/add-to-audacious2-playlist.py default-pyextensions/meld-compare.py default-pyextensions/kdiff3-compare.py default-pyextensions/open-as-root.py default-pyextensions/open-terminal-geometry.py default-pyextensions/replace-in-filenames.py default-pyextensions/set-as-desktop-background.py
